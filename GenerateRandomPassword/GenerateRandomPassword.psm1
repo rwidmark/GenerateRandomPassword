b793131e5752d1d4df8f1693cc53944a023b5426
Robin Widmark <robin@widmark.dev>
2025-05-21 16:50:23 +0200

    update

--- GenerateRandomPassword/GenerateRandomPassword.psm1
+++ GenerateRandomPassword/GenerateRandomPassword.psm1
@@ -1,8 +1,8 @@
 ﻿<#
 MIT License
 
-Copyright (C) 2023 Robin Stolpe.
-<https://stolpe.io>
+Copyright (C) 2025 Robin Widmark.
+<https://widmark.dev>
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
@@ -49,13 +49,14 @@ function New-RSRandomPassword {
         # Returns a random password that are 20 characters long and contains 4 special character.
 
         .NOTES
-        Author:     Robin Stolpe
-        Mail:       robin@stolpe.io
-        Website:	https://stolpe.io
-        GitHub:		https://github.com/rstolpe
-        Twitter:	https://twitter.com/rstolpes
-        PSGallery:	https://www.powershellgallery.com/profiles/rstolpe
-        NuGet: 		https://www.nuget.org/profiles/rstolpe
+        Author:         Robin Widmark
+        Mail:           robin@widmark.dev
+        Website/Blog:   https://widmark.dev
+        X:              https://x.com/widmark_robin
+        Mastodon:       https://mastodon.social/@rwidmark
+		YouTube:		https://www.youtube.com/@rwidmark
+        Linkedin:       https://www.linkedin.com/in/rwidmark/
+        GitHub:         https://github.com/rwidmark
     #>
 
     [CmdletBinding()]
